f1c2a73bac326d610545c56970f2df473d12afa4
ShvaichVictoria <byvica@gmail.com>
2025-12-12 11:27:11 +0300

    add dist

--- js/navBar.js
+++
@@ -1,9 +0,0 @@
-let btnBar = document.querySelector('.nav-button');
-let navBar= document.querySelector('.navigationBarModal');
-let btnClose= document.querySelector('.btnClose');
-btnBar.addEventListener('click',function(){
-  navBar.style.display = 'block';
-})
-btnClose.addEventListener('click',function(){
-  navBar.style.display = 'none';
-})
